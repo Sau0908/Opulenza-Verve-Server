c752d88b80f4c1ab4f2095eab041ef64e8209a54
Sau0908 <kashyapsaurabh0908@gmail.com>
2024-07-02 16:28:55 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,152 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import bodyParser from "body-parser";
+import { MongoClient, ServerApiVersion, ObjectId } from 'mongodb';
+import dotenv from 'dotenv';
+
+
+import reservationRoutes from "./routes/reservation.js";
+import contactRoutes from "./routes/contact.js";
+import jwt from 'jsonwebtoken';
+
+const app = express();
+app.use(bodyParser.json());
+const PORT = process.env.PORT || 5000;
+dotenv.config();
+app.use(cors());
+app.use(express.json())
+app.use("/api/reservation", reservationRoutes);
+app.use("/api/contact", contactRoutes);
+
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' });
+    }
+    // bearer token
+    const token = authorization.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: true, message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dkm5by0.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+
+        const menuCollection = client.db('opulenza').collection('menu')
+        const userCollection = client.db('opulenza').collection('users')
+
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email }
+            const user = await userCollection.findOne(query);
+            if (user?.role !== 'admin') {
+                return res.status(403).send({ error: true, message: 'forbidden message' });
+            }
+            next();
+        }
+
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ token })
+        })
+
+        app.get('/menu', async (req, res) => {
+            const result = await menuCollection.find().toArray();
+            res.send(result)
+        })
+
+        app.post('/menu', async (req, res) => {
+            const menu = req.body;
+            console.log(menu);
+            const result = await menuCollection.insertOne(menu);
+            res.send(result)
+        })
+
+        app.get('/users', async (req, res) => {
+            const result = await userCollection.find().toArray();
+            res.send(result)
+        })
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+
+            const query = { email: user.email }
+            const existingUser = await userCollection.findOne(query)
+
+            if (existingUser) {
+                return res.send({ message: 'user already exist' })
+            }
+            const result = await userCollection.insertOne(user)
+            res.send(result)
+        })
+
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await userCollection.deleteOne(query);
+            res.send(result)
+        })
+
+        app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+
+            if (req.decoded.email !== email) {
+                res.send({ admin: false })
+            }
+            const query = { email: email }
+            const user = await userCollection.findOne(query)
+            const result = { admin: user?.role === 'admin' }
+            res.send(result)
+        })
+
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    role: 'admin'
+                },
+            }
+            const result = await userCollection.updateOne(filter, updateDoc)
+            res.send(result)
+            console.log(result);
+        })
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Restaurant ongoing!')
+})
+
+mongoose
+    .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() =>
+        app.listen(PORT, () => {
+            console.log(`server running on port ${PORT}`);
+        })
+    )
---
+++ models/bookingTable.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const bookingSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        required: true,
+    },
+    selectedHour: {
+        type: String,
+        required: true,
+    },
+    partySize: {
+        type: Number,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        // Assuming email should be unique
+        trim: true, // Remove leading/trailing spaces
+        lowercase: true, // Store email in lowercase
+    },
+    phone: {
+        type: String,
+        required: true,
+    },
+    selectedPeriod: {
+    type: String,
+    reuired: true,
+  },
+});
+
+export default mongoose.model("Booking", bookingSchema);
